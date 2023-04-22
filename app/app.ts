f26a57dec22b01757f2e65b11fb1e5393b0b26a2
Andres Ladino <aladinoster@gmail.com>
2023-04-22 18:29:55 +0000

    Add: Basic typing

--- app/app.ts
+++ app/app.ts
@@ -1,7 +1,16 @@
 function startGame() {
     // starting a new game
+
+    let playerName: string = 'Audrey';
+    playerName = 'Andres';
+    logPlayer(playerName);
+
     var messagesElement = document.getElementById('messages');
     messagesElement!.innerText = 'Welcome to MultiMath! Starting new Game Andy...!';
+}
+
+function logPlayer(name) {
+    console.log(`New game starting for player: ${name}`);
 }
 
 document.getElementById('startGame')!.addEventListener('click', startGame);
--- js/app.js
+++ js/app.js
@@ -1,7 +1,12 @@
-"use strict";
 function startGame() {
+    var playerName = 'Audrey';
+    playerName = 'Andres';
+    logPlayer(playerName);
     var messagesElement = document.getElementById('messages');
     messagesElement.innerText = 'Welcome to MultiMath! Starting new Game Andy...!';
+}
+function logPlayer(name) {
+    console.log("New game starting for player: ".concat(name));
 }
 document.getElementById('startGame').addEventListener('click', startGame);
 //# sourceMappingURL=app.js.map
